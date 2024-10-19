bbbcab88bcf7961e53684122155003599727d3eb
Bladimir Gonzales <136342516+Dart5045@users.noreply.github.com>
2024-10-19 15:17:42 +0200

    Update 02-xargs.py

--- Semana 2/python/03-functions/02-xargs.py
+++ Semana 2/python/03-functions/02-xargs.py
@@ -1,3 +1,8 @@
+"""
+El principal uso de *args y **kwargs es en la definición de funciones. Ambos permiten pasar un número variable de argumentos a una función, 
+por lo que si quieres definir una función cuyo número de parámetros de entrada puede ser variable, considera el uso de *args o **kwargs como una opción.
+"""
+
 def suma(x, y):
     """ Función clásica con 2 parámetros """
     print(x+y)
