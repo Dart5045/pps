7ca5cfc2f7521dc8314ed13f3f217d8bfe5ea2de
Bladimir Gonzales <136342516+Dart5045@users.noreply.github.com>
2024-10-19 15:18:49 +0200

    Update 01-intro.py

--- Semana 2/python/05-exceptions/01-intro.py
+++ Semana 2/python/05-exceptions/01-intro.py
@@ -1,3 +1,10 @@
+"""
+Las excepciones en Python son una herramienta muy potente que la gran mayoría de lenguajes de programación modernos tienen. 
+Se trata de una forma de controlar el comportamiento de un programa cuando se produce un error.
+Esto es muy importante ya que salvo que tratemos este error, el programa se parará, y esto es algo que en determinadas aplicaciones no es una opción válida.
+"""
+
+
 try:
     n1 = int(input("Ingresar primer valor numérico: "))
 except:
